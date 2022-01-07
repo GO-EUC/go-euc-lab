5cc9a4d2f1e3be8000f8daab658994793628fd8c
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2022-01-07 15:45:18 +0100

    Added outputs for Ansible

---
+++ terraform/azure/outputs.tf
@@ -0,0 +1,22 @@
+output "vault" {
+    description = "The name of the Azure Key Vault"
+    value = azurerm_key_vault.vault.name
+}
+
+output "vault_resource_group" {
+    description = "The resource group name of the Azure Key Vault"
+    value = azurerm_key_vault.vault.resource_group_name
+}
+
+output "admin" {
+    description = "Local administrator account name"
+    value = azurerm_key_vault_secret.admin.name
+}
+
+output "dc" {
+    value = module.ActiveDirectory.vm_info
+}
+
+output "cc" {
+    value = module.CitrixCloudConnectors.vm_info
+}
