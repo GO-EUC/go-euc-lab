a86ed60f80bb2a9b19252855de062c08d62d5093
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2022-01-08 15:56:58 +0100

    Generate inventory file with the pipeline for Ansible

---
+++ ansible/powershell/GenerateInventory.ps1
@@ -0,0 +1,45 @@
+param (
+    [Parameter()]
+    [string]
+    $StateFile,
+
+    [Parameter()]
+    [string]
+    $InventoryFile  
+)
+
+$state = Get-Content -Path $StateFile -Raw | ConvertFrom-Json
+
+$machines = $state.outputs.PSObject.Properties | Where-Object {$_.Name -ne "admin" -and $_.Name -ne "vault" -and $_.Name -ne "vault_resource_group" } | Select-Object Name
+
+$content = @()
+foreach ($machine in $machines) {
+    $content += "[$($machine.Name)]"
+
+    $machineProperties = $state.outputs.$($machine.Name).Value
+
+    foreach ($machineProperty in $machineProperties) {
+        $content += $machineProperty
+    }
+
+    $content += ""
+}
+
+$content += "[all:vars]"
+$content += "vault_name=$($state.outputs.vault.value)"
+$content += "resource_group=$($state.outputs.vault_resource_group.value)"
+$content += "ansible_user=$($state.outputs.admin.value)"
+
+$inventoryDir = $InventoryFile.Substring(0,$InventoryFile.LastIndexOf("/"))
+if ($inventoryDir -ne ".") {
+    
+    if ((Test-Path -Path $inventoryDir) -eq $false) {
+        try {
+            New-Item -Path $inventoryDir -ItemType Directory -Force | Out-Null
+        } catch {
+            throw "Error while creating directory: $($_.Exception.Message)"
+        }
+    }
+}
+
+Set-Content -Path $InventoryFile -Value $content
