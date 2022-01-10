ba9eae69291bb6052fc540af6e59d7990736c176
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2022-01-10 08:53:20 +0100

    Used the wrong address for the DNS, so restoring to initial

--- terraform/azure/vnet.tf
+++ terraform/azure/vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "AzurevNet" {
   name          = "vnet-infra-${local.environment_abbreviations[terraform.workspace]}"
   address_space = [local.infra_cidr[terraform.workspace]]
-  dns_servers   = [cidrhost(local.infra_cidr[terraform.workspace], 10), "168.63.129.16"]
+  dns_servers   = [cidrhost(local.infra_subnet_cidr[terraform.workspace], 10)]
 
   location            = azurerm_resource_group.VNet.location
   resource_group_name = azurerm_resource_group.VNet.name
