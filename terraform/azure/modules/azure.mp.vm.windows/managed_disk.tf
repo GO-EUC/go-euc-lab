5fdf8630acc3a84c3261cb8c22a28df29443b01f
Anton van Pelt <anton@antonvanpelt.com>
2022-02-16 20:14:50 +0100

    20gb

--- terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
+++ terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
@@ -4,5 +4,5 @@ resource "azurerm_managed_disk" "manageddisk" {
   resource_group_name = var.azure_resource_group_name
   storage_account_type = "Standard_LRS"
   create_option = "Empty"
-  disk_size_gb = "10"
+  disk_size_gb = "20"
 }  
