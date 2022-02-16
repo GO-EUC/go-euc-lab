49528cf4e07c8b5cd2b2516d4f52a938d0bba6fa
Anton van Pelt <anton@antonvanpelt.com>
2022-02-16 11:36:24 +0100

    update

--- terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
+++ terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
@@ -1,6 +1,5 @@
 resource "azurerm_managed_disk" "manageddisk" {
-  count = var.vm_count
-  name = "${var.vm_name}${count.index + 1}-manageddisk"
+  name = "${var.vm_name}-manageddisk"
   location = var.azure_location
   resource_group_name = var.azure_resource_group_name
   storage_account_type = "Standard_LRS"
