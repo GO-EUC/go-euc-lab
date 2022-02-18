04d67354c4ac2af8b37c050b0458206744d53d1d
Anton van Pelt <anton@antonvanpelt.com>
2022-02-18 23:26:39 +0100

    Revert "update"
    This reverts commit fc3b844a1ae7484ec52b15241cf209e7397e328b.

--- terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
+++ terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
@@ -1,7 +1,7 @@
 resource "azurerm_managed_disk" "manageddisk" {
   count                 = var.managed_disk_count
-  vmcount              = var.vm_count
-  name                  = "${var.vm_name}${vmcount.index + 1}-manageddisk${count.index + 1}"
+  name                  = "${var.vm_name}-manageddisk${count.index + 1}"
+  #name = "${var.vm_name}-manageddisk"
   location = var.azure_location
   resource_group_name = var.azure_resource_group_name
   storage_account_type = "Standard_LRS"
