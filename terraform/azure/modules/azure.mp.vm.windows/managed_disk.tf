13197b574ae1a4985205808f6bb72b00e9c42a5e
Anton van Pelt <anton@antonvanpelt.com>
2022-02-18 23:51:29 +0100

    no message

--- terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
+++ terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
@@ -1,7 +1,7 @@
 resource "azurerm_managed_disk" "manageddisk" {
   vmcount               = var.vm_count
   count                 = var.managed_disk_count
-  name                  = "${var.vm_name}${vmcount}-manageddisk${count.index + 1}"
+  name                  = "${var.vm_name}${vmcount.index + 1}-manageddisk${count.index + 1}"
   #name = "${var.vm_name}-manageddisk"
   location = var.azure_location
   resource_group_name = var.azure_resource_group_name
