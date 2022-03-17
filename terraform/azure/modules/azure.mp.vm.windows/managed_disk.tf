9e918b28a9171e32682eed30cd1e84d24e344278
Anton van Pelt <anton@antonvanpelt.com>
2022-03-17 13:48:19 +0100

    conditional 3

--- terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
+++ terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
@@ -1,5 +1,5 @@
 resource "azurerm_managed_disk" "manageddisk" {
-  count                 = var.managed_disk_enabled ? yes : no
+  count                 = var.managed_disk_enabled ? 1 : 0
   name                  = "${var.vm_name}-${var.vm_count}-manageddisk"
   location = var.azure_location
   resource_group_name = var.azure_resource_group_name
