1dac0c9d94ba52d30af5973699f2c3650cbb6c72
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2022-04-01 09:56:49 +0200

    Minor cleanup

--- terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
+++ terraform/azure/modules/azure.mp.vm.windows/managed_disk.tf
@@ -1,9 +1,9 @@
 resource "azurerm_managed_disk" "manageddisk" {
   count                 = var.managed_disk_enabled != null ? 1 : 0
   name                  = "${var.vm_name}-${var.vm_count}-manageddisk"
-  location = var.azure_location
-  resource_group_name = var.azure_resource_group_name
-  storage_account_type = "Standard_LRS"
-  create_option = "Empty"
-  disk_size_gb = "20"
+  location              = var.azure_location
+  resource_group_name   = var.azure_resource_group_name
+  storage_account_type  = "Standard_LRS"
+  create_option         = "Empty"
+  disk_size_gb          = "20"
 }  
