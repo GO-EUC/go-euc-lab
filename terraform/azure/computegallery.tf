15725cf726342d67b5446f93181cbed162398053
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2022-05-21 16:50:19 +0200

    Trying to get this working again

--- terraform/azure/computegallery.tf
+++ terraform/azure/computegallery.tf
@@ -5,8 +5,8 @@ resource "azurerm_shared_image_gallery" "azurecomputegallery" {
   description         = "Shared images and things."
 }
 
-resource "azurerm_shared_image" "image" {
-  name                   = "Windows10-21h2"
+resource "azurerm_shared_image" "image_avd" {
+  name                   = "Windows10-21h2-avd"
   gallery_name           = azurerm_shared_image_gallery.azurecomputegallery.name
   resource_group_name    = azurerm_resource_group.EUCWorkers.name
   location               = azurerm_resource_group.EUCWorkers.location
@@ -19,5 +19,22 @@ resource "azurerm_shared_image" "image" {
     publisher = "microsoftwindowsdesktop"
     offer     = "Windows-10"
     sku       = "win10-21h2-avd-g2"
+  }
+}
+
+resource "azurerm_shared_image" "image_citrix" {
+  name                   = "Windows10-21h2-citrix"
+  gallery_name           = azurerm_shared_image_gallery.azurecomputegallery.name
+  resource_group_name    = azurerm_resource_group.EUCWorkers.name
+  location               = azurerm_resource_group.EUCWorkers.location
+  os_type                = "Windows"
+  specialized            = false
+  hyper_v_generation     = "V2"
+  trusted_launch_enabled = false
+
+  identifier {
+    publisher = "microsoftwindowsdesktop"
+    offer     = "Windows-10"
+    sku       = "win10-21h2-citrix-g2"
   }
 }
