68eca54e96b4c6a7e59aff56dc74a743b89b2f92
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2022-07-21 16:45:55 +0200

    Disable the mgnt deployment

--- terraform/vmware/resource.tf
+++ terraform/vmware/resource.tf
@@ -18,28 +18,28 @@ module "ActiveDirectory" {
   vsphere_source_template = local.vsphere_source_template_windows
 }
 
-module "ManagementServer" {
-  source    = "./modules/vmware.vsphere.vm.windows"
-  vm_name   = "${local.environment_abbreviations[terraform.workspace]}-mgnt"
-  vm_cpu    = 4
-  vm_memory = 4096
-  vm_disks = [{
-    unit_number = 0
-    label       = "disk0"
-    size        = 128
-  }]
-  local_admin_password  = var.local_admin_password
-  domain                = local.ad_domain_fqdn
-  domain_admin          = var.domain_admin
-  domain_admin_password = var.domain_admin_password
+# module "ManagementServer" {
+#   source    = "./modules/vmware.vsphere.vm.windows"
+#   vm_name   = "${local.environment_abbreviations[terraform.workspace]}-mgnt"
+#   vm_cpu    = 4
+#   vm_memory = 4096
+#   vm_disks = [{
+#     unit_number = 0
+#     label       = "disk0"
+#     size        = 128
+#   }]
+#   local_admin_password  = var.local_admin_password
+#   domain                = local.ad_domain_fqdn
+#   domain_admin          = var.domain_admin
+#   domain_admin_password = var.domain_admin_password
 
-  virtual_network_portgroup_name = local.virtual_network_portgroup_name[terraform.workspace]
+#   virtual_network_portgroup_name = local.virtual_network_portgroup_name[terraform.workspace]
 
-  vsphere_datacenter      = local.vsphere_datacenter
-  vsphere_datastore       = local.vsphere_datastore
-  vsphere_cluster         = local.vsphere_cluster
-  vsphere_source_template = local.vsphere_source_template_windows
-}
+#   vsphere_datacenter      = local.vsphere_datacenter
+#   vsphere_datastore       = local.vsphere_datastore
+#   vsphere_cluster         = local.vsphere_cluster
+#   vsphere_source_template = local.vsphere_source_template_windows
+# }
 
 # module "FileServer" {
 #   source    = "./modules/vmware.vsphere.vm.windows"
