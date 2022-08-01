3ca2bd065b063e7484b0dedc798a0489fc7b0fd5
Sven Jansen <sven.jansen@devicetrust.com>
2022-08-01 10:22:21 +0200

    Added modules 05-07 to run.sh.

--- terraform/citrix-adc/run.sh
+++ terraform/citrix-adc/run.sh
@@ -2,4 +2,7 @@ terraform init &&
 terraform apply --auto-approve -target="module._01_build" && sleep 5m && 
 terraform apply --auto-approve -target="module._02_reset_password" && sleep 10s && 
 terraform apply --auto-approve -target="module._03_license" && sleep 2m && 
-terraform apply --auto-approve -target="module._04_base"
+terraform apply --auto-approve -target="module._04_base" && sleep 5s && 
+terraform apply --auto-approve -target="module._05_ssl" && sleep 5s && 
+terraform apply --auto-approve -target="module._06_lb" && sleep 5s && 
+terraform apply --auto-approve -target="module._07_gw"
