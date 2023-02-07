f36729d85e819500e98921f61e0ea04a8a465390
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2023-02-07 08:52:14 +0100

    Ensure to set the token before creating the correct vault and adding passwords

--- init/init.ps1
+++ init/init.ps1
@@ -264,12 +264,15 @@ if ($vaultStatus.sealed -eq $true) {
     }
 }
 
+# Set the auth token for the Vault
+$env:VAULT_TOKEN=$($vaultInit.root_token)
+
 # Enable kv vault
-& "$env:TEMP\Hashicorp\vault.exe" secrets enable -version=1 -path go kv | Out-Null
+& "$env:TEMP\Hashicorp\vault.exe" secrets enable -version=1 -path=go kv | Out-Null
 
 # Adding secrets to the vault
 & "$env:TEMP\Hashicorp\vault.exe" kv put -mount=go domain name=$($settings.domain_name)
-& "$env:TEMP\Hashicorp\vault.exe" kv put -mount=go vmware/esx/$($esx_host_name) password=$($unsecureEsxPassword) user=$($settings.esx_username) ip=$($esx_host_ip) name=$($esx_host_name) datastore=$($settings.esx_datastore) network=$($settings.esx_network)
+& "$env:TEMP\Hashicorp\vault.exe" kv put -mount=go vmware/esx/$($settings.esx_host_name) password=$($unsecureEsxPassword) user=$($settings.esx_username) ip=$($settings.esx_host_ip) name=$($settings.esx_host_name) datastore=$($settings.esx_datastore) network=$($settings.esx_network)
 & "$env:TEMP\Hashicorp\vault.exe" kv put -mount=go vmware/vcsa ip=$($settings.vcsa_ip) name=$($settings.vcsa_name)
 & "$env:TEMP\Hashicorp\vault.exe" kv put -mount=go vmware/network network_cidr=$($settings.network_cidr) network_gateway=$($settings.network_gateway) network_dns=$($settings.network_dns)
 & "$env:TEMP\Hashicorp\vault.exe" kv put -mount=go docker password=$randomPassword user=$($settings.docker_username) ip=$($dockerIp) name=$($settings.docker_name)
