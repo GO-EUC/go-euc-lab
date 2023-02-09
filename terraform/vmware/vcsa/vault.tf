f49b13a21997cf2ac7d20c86016392043bc309f6
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2023-02-09 10:42:39 +0100

    Correct path

--- terraform/vmware/vcsa/vault.tf
+++ terraform/vmware/vcsa/vault.tf
@@ -3,6 +3,6 @@ data "vault_kv_secret" "vcsa" {
 }
 
 resource "vault_kv_secret" "secret" {
-    path = data.vault_kv_secret.vcsa
+    path = data.vault_kv_secret.vcsa.path
     data_json = jsonencode(merge( data.vault_kv_secret.data_json, { password = random_password.password.result }))
 }
