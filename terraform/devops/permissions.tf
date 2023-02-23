f56f3d051477f9a93b39a83ded436acfa7609dea
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2023-02-23 17:00:58 +0100

    Ensure the permissions are set so the pipeline can be tirggert via the api

---
+++ terraform/devops/permissions.tf
@@ -0,0 +1,23 @@
+data "azuredevops_users" "svc" {
+    subject_types = ["svc"]
+    origin = "vsts"
+}
+
+locals {
+        build_service = [
+        for x in data.azuredevops_users.svc.users :
+        x
+        if can(regex("${azuredevops_project.project.name} Build Service", x.display_name))
+    ]
+}
+
+data "azuredevops_group" "administrators" {
+    project_id = azuredevops_project.project.id
+    name       = "Build Administrators"
+}
+
+resource "azuredevops_group_membership" "member" {
+    group = data.azuredevops_group.administrators.descriptor
+    
+    members = local.build_service[*].descriptor
+}
