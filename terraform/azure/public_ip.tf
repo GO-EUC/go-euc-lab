f7b12d4111920383e3ebd2ce01aeded47ec98c72
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2025-08-29 17:21:33 +0200

    Remove old shizzle

--- terraform/azure/public_ip.tf
+++
@@ -1,8 +0,0 @@
-#PIP used for Azure Bastion
-resource "azurerm_public_ip" "pip-bastion" {
-  name                = "pip-bastion-${local.environment_abbreviations[terraform.workspace]}"
-  resource_group_name = azurerm_resource_group.Bastion.name
-  location            = var.azure_region
-  allocation_method   = "Static"   #Static only allowed by standard sku
-  sku                 = "Standard" #Alligning basic SKU, this is cheaper
-}
--- terraform/azure/vnet.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_virtual_network" "AzurevNet" {
-  name          = "vnet-infra-${local.environment_abbreviations[terraform.workspace]}"
-  address_space = [local.infra_cidr[terraform.workspace]]
-  dns_servers   = [cidrhost(local.infra_subnet_cidr[terraform.workspace], 10)]
-
-  location            = azurerm_resource_group.VNet.location
-  resource_group_name = azurerm_resource_group.VNet.name
-}
