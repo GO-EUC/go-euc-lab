1d060122f177a0a98f88de3d6bcb6da44a0b42c9
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2025-10-01 17:46:05 +0200

    Formatting and added ansible

--- terraform/vmware/infra/main.tf
+++ terraform/vmware/infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
 
-  required_version = ">= 1.2" 
-  
+  required_version = ">= 1.2"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -9,25 +9,30 @@ terraform {
     }
 
     vsphere = {
-      source = "hashicorp/vsphere"
+      source  = "hashicorp/vsphere"
       version = "~>2.2"
+    }
+
+    ansible = {
+      source  = "ansible/ansible"
+      version = "~>1.3.0"
     }
   }
 
-    backend "pg" { 
-        schema_name = "infra"
-    }
+  backend "pg" {
+    schema_name = "infra"
+  }
 }
 
 provider "vsphere" {
-    user           = jsondecode(data.vault_kv_secret.vcsa.data_json).user
-    password       = jsondecode(data.vault_kv_secret.vcsa.data_json).password
-    vsphere_server = cidrhost(jsondecode(data.vault_kv_secret.network.data_json).cidr ,jsondecode(data.vault_kv_secret.vcsa.data_json).ip)
+  user           = jsondecode(data.vault_kv_secret.vcsa.data_json).user
+  password       = jsondecode(data.vault_kv_secret.vcsa.data_json).password
+  vsphere_server = cidrhost(jsondecode(data.vault_kv_secret.network.data_json).cidr, jsondecode(data.vault_kv_secret.vcsa.data_json).ip)
 
-    allow_unverified_ssl = true
+  allow_unverified_ssl = true
 }
 
 provider "vault" {
-    address = var.vault_address
-    token   = var.vault_token
+  address = var.vault_address
+  token   = var.vault_token
 }
