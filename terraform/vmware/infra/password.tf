33131c31543ada20852f077f4360b69fe19ad37f
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2025-10-01 17:45:51 +0200

    Formatting

--- terraform/vmware/infra/password.tf
+++ terraform/vmware/infra/password.tf
@@ -1,9 +1,9 @@
 resource "random_password" "password" {
-    length           = 10
-    min_lower        = 1
-    min_upper        = 1
-    min_special      = 1
-    min_numeric      = 1
-    special          = true
-    override_special = "!#$%&"
+  length           = 10
+  min_lower        = 1
+  min_upper        = 1
+  min_special      = 1
+  min_numeric      = 1
+  special          = true
+  override_special = "!#$%&"
 }
