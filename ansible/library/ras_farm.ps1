63f3cd9f0360ba323859bbc4d92c4e5a9db0887b
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2025-12-16 15:54:40 +0100

    Added final version of Parallels with a setup with vsphere

--- ansible/library/ras_farm.ps1
+++ ansible/library/ras_farm.ps1
@@ -12,6 +12,13 @@ $debug = $debug_level -gt 2
 $gateway = Get-AnsibleParam $params "gateway" -type "str" -FailIfEmpty $true
 $username = Get-AnsibleParam $params "username" -type "str" -FailIfEmpty $true
 $password = Get-AnsibleParam $params "password" -type "str" -FailIfEmpty $true
+
+$provider_type = Get-AnsibleParam $params "provider_type" -type "str" -Default "VCenter"
+$provider_server = Get-AnsibleParam $params "provider_server" -type "str" -FailIfEmpty $true
+$provider_username = Get-AnsibleParam $params "provider_username" -type "str" -FailIfEmpty $true
+$provider_password = Get-AnsibleParam $params "provider_password" -type "str" -FailIfEmpty $true
+$provider_version = Get-AnsibleParam $params "provider_version" -type "str" -Default "v8_0"
+
 $state = Get-AnsibleParam $params "state" -type "str" -Default "present"
 
 $result = @{
@@ -32,6 +39,7 @@ if ($state -eq "absent") {
     Fail-Json $result "Fail to setup new RAS Session. Error: $($_)"
   }
 
+  ## Remove Gateway if exists
   try {
     $listGateway = Get-RASGateway
 
@@ -39,6 +47,24 @@ if ($state -eq "absent") {
 
     if ($filterGateway) {
       $filterGateway | Remove-RASGateway
+
+      Invoke-RASApply
+      $result.changed = $true
+    }
+  } catch {
+    Fail-Json $result "Fail to create new RAS Gateway. Error: $($_)"
+  }
+
+  ## Remove Provider if exists
+    ## Create Provider if not exists
+  try {
+    $listProvider = Get-RASProvider
+    $filterProvider = $listProvider | Where-Object { $_.Server -eq $provider_server }
+
+    if ($filterProvider) {
+      $filterProvider | Remove-RASProvider
+
+      Invoke-RASApply
       $result.changed = $true
     }
   } catch {
@@ -53,6 +79,7 @@ if ($state -eq "absent") {
     Fail-Json $result "Fail to setup new RAS Session. Error: $($_)"
   }
 
+  ## Create Gateway if not exists
   try {
     $listGateway = Get-RASGateway
 
@@ -60,11 +87,31 @@ if ($state -eq "absent") {
 
     if (!$filterGateway) {
       New-RASGateway -Server $gateway -NoRestart
+
+      Invoke-RASApply
       $result.changed = $true
     }
   } catch {
     Fail-Json $result "Fail to create new RAS Gateway. Error: $($_)"
   }
+
+  ## Create Provider if not exists
+  try {
+    $listProvider = Get-RASProvider
+    $filterProvider = $listProvider | Where-Object { $_.Server -eq $provider_server }
+
+    if (!$filterProvider) {
+
+      $securePassword = ConvertTo-SecureString -String $provider_password -AsPlainText -Force
+      New-RASProvider -Server $provider_server -VCenter -VCenterVersion $provider_version -NoRestart -ProviderUsername $provider_username -ProviderPassword $securePassword
+
+      Invoke-RASApply
+      $result.changed = $true
+    }
+  } catch {
+    Fail-Json $result "Fail to create new RAS Gateway. Error: $($_)"
+  }
+
 }
 
 Exit-Json $result
